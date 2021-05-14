0dbe6f89c5830711475705e6e578f5f1143f49cf
Karsten Jepsen <kjepsen86@gmail.com>
2021-05-14 10:56:04 -0500

    imported components, syntax improvements

--- src/components/favoriteView/favoriteView.jsx
+++ src/components/favoriteView/favoriteView.jsx
@@ -3,18 +3,31 @@ import {useSelector } from 'react-redux';
 
 function favoriteView() {
 
-    const favoriteReducer = useSelector(store => store.favoriteReducer);
+    const dispatch = useDispatch();
+    const [category, setCategory] = useState('');
+    const favorite = useSelector(store => store.favorite);
+    dispatch( {type: 'SET_CATEGORY', payload: category});
+
 
     return (
-        <>
+       
             <div>
-                {favoriteReducer.map((gif, i) => {
+                {favorite.map((gif, i) => {
                     return (
-                        <img key={i} src={gif.url}>{gif.category}</img>
+                        <>
+                            <img key={i} src={giphy.data.images.original.url}></img>
+                            <select className="category" placeholder="Choose Category" 
+                                onChange={(event) => setCategory(event.target.value)}>
+                                <option value="funny">Funny</option>
+                                <option value="cohort">Cohort</option>
+                                <option value="cartoon">Cartoon</option>
+                                <option value="nsfw">NSFW</option>
+                                <option value="meme">Meme</option>
+                            </select>
+                        </>
                     )
                 })}
-            </div>
-        </>
+            </ div>
     )
 }
 

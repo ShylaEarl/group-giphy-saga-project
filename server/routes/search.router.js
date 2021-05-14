8753ea8e9833dd45a8110bcba5cbc5ffeb550125
eyramtay <71682389+eyramtay@users.noreply.github.com>
2021-05-14 11:21:33 -0500

    updated search router

--- server/routes/search.router.js
+++ server/routes/search.router.js
@@ -7,3 +7,18 @@ const axios = require('axios');
 require('dotenv').config();
 
 
+router.get('/', (req, res) => {
+    console.log('In GET');
+    // API call to giphy's server with our API key
+    axios.get( `http://api.giphy.com/v1/gifs/search?api_key=${GIPHY_KEY}` )
+    .then( response => {
+        console.log('Data from giphy', response.data);
+        res.send(response.data);
+    })
+    .catch( error => {
+        console.log('Error getting searched giphy', error);
+        res.sendStatus(500);
+    })
+});
+
+module.exports = router;
